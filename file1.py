4e9d0319c2e3ea7579c600ff3bfb70d77aa88b56
shashankDakre <138364767+shashankDakre@users.noreply.github.com>
2024-09-09 12:54:31 -0700

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,48 @@
+############String#############
+a="shashank"
+# print(a[2])
+print(a[1:3])
+s="Certification"
+print(len(s))
+print(s[2])
+print(s[-11])
+print(s[2:5])
+print(s[-11:-8])
+print(s[9:13])
+print(s[:4])
+print(s[:5:2])
+print(s[5::3])
+print(s[::4])
+print(s[::-1])
+print(s[::-2])
+a=input("Enter any string=")
+print(a[::-1])
+print(len(a))
+print(a.split("?"))
+s="shashank"
+print(s.capitalize())
+print(s.lower())
+print(s.upper())
+print(s.find("n"))
+print(s.count("s"))
+print(s.replace("s","v"))
+print(s.endswith("s"))
+###wap to accept a string and display it with by good morning
+s=input("Enter a name = ")
+print ("good morning", s)
+### wap to accept a string having two space betw words & display by removing one space
+s=input("Enter a name = ")
+print (s.replace("  "," "))
+###wap to accept string display in
+reverse , display last char , no. of time "i" occured , in lower case , upper , capitalize , loc of like , replace python with sql
+s = input("Enter a string=")
+print(s[::-1])
+print(s.capitalize())
+print(s.lower())
+print(s.upper()) 
+print(s.find("like"))
+print(s.count("i"))
+print(s.replace("python","sql"))
+print(s[:-9])
+s="hello12"
+  
